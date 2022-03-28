0539be02fdb4a2276a749438506dcfda66284a19
bfccardoso <bfccardoso@gmail.com>
2022-03-28 16:53:36 -0300

    Create input component

---
+++ components/input.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Input = ({...children}) => {
+    return (
+        <div className='w-full px-3'>
+            <label className="text-gray-700 text-sm font-bold">{children.label}</label>
+            <input
+                className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                {...children}
+            />
+            {children.errorMessage && <p className="text-red-500 text-xs italic">{children.errorMessage}</p>}
+        </div>
+    )
+}
+
+export default Input
